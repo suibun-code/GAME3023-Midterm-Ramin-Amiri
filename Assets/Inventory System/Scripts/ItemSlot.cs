348669733b3c87da15eda6e2cb46e5e34ee592cb
Ramin Amiri <zeuspsn.55555@gmail.com>
2020-10-28 10:02:39 -0400

    Dragging items that are inside inventory

---
+++ Assets/Inventory System/Scripts/ItemSlot.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : EventTrigger
+{
+    // Event callbacks
+    public UnityEvent<Item> onItemUse;
+
+    // flag to tell ItemSlot it needs to update itself after being changed
+    private bool b_needsUpdate = true;
+
+    // Declared with auto-property
+    public Item ItemInSlot { get; private set; }
+    public int ItemCount { get; private set; }
+
+
+    // scene references
+    [SerializeField]
+    private TMPro.TextMeshProUGUI itemCountText;
+
+    [SerializeField]
+    private Image itemIcon;
+
+    protected RectTransform rectTransform = null;
+    private Vector3 originalTransform = Vector3.zero;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        originalTransform = itemIcon.transform.position;
+    }
+
+    private void Update()
+    {
+        if (b_needsUpdate)
+        {
+            UpdateSlot();
+        }
+    }
+
+    /// <summary>
+    /// Returns true if there is an item in the slot
+    /// </summary>
+    /// <returns></returns>
+    public bool HasItem()
+    {
+        return ItemInSlot != null;
+    }
+
+    /// <summary>
+    /// Removes everything in the item slot
+    /// </summary>
+    /// <returns></returns>
+    public void ClearSlot()
+    {
+        ItemInSlot = null;
+        b_needsUpdate = true;
+    }
+
+    /// <summary>
+    /// Attempts to remove a number of items. Returns number removed
+    /// </summary>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public int TryRemoveItems(int count)
+    {
+        if (count > ItemCount)
+        {
+            int numRemoved = ItemCount;
+            ItemCount -= numRemoved;
+            b_needsUpdate = true;
+            return numRemoved;
+        }
+        else
+        {
+            ItemCount -= count;
+            b_needsUpdate = true;
+            return count;
+        }
+    }
+
+    /// <summary>
+    /// Sets what is contained in this slot
+    /// </summary>
+    /// <param name="item"></param>
+    /// <param name="count"></param>
+    public void SetContents(Item item, int count)
+    {
+        ItemInSlot = item;
+        ItemCount = count;
+        b_needsUpdate = true;
+    }
+
+    /// <summary>
+    /// Activate the item currently held in the slot
+    /// </summary>
+    public void UseItem()
+    {
+        // if(ItemInSlot != null)
+        // {
+        //     if(ItemCount >= 1)
+        //     {
+        //         ItemInSlot.Use();
+        //         onItemUse.Invoke(ItemInSlot);
+        //         ItemCount--;
+        //         b_needsUpdate = true;
+        //     }
+        // }
+    }
+
+    /// <summary>
+    /// Update visuals of slot to match items contained
+    /// </summary>
+    private void UpdateSlot()
+    {
+        if (ItemCount == 0)
+        {
+            ItemInSlot = null;
+        }
+
+        if (ItemInSlot != null)
+        {
+            itemCountText.text = ItemCount.ToString();
+            itemIcon.sprite = ItemInSlot.Icon;
+            itemIcon.gameObject.SetActive(true);
+        }
+        else
+        {
+            itemIcon.gameObject.SetActive(false);
+        }
+
+        b_needsUpdate = false;
+    }
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+
+        itemIcon.GetComponent<Canvas>().sortingOrder = 2;
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+
+        itemIcon.transform.position += (Vector3)eventData.delta;
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+
+        itemIcon.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+        itemIcon.GetComponent<Canvas>().sortingOrder = 1;
+    }
+}
